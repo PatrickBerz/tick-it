2ef9ce727f839e190ef2e7f147011fd1068e504f
PatrickB <patrickberzins14@gmail.com>
2023-03-02 16:41:42 -0600

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+        <Button variant='warning'>Admin</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Tick-it app
