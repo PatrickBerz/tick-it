ab00bd8c2b64c39ead2bf9eba1438f5b4ea6df0c
PatrickB <patrickberzins14@gmail.com>
2023-03-07 20:04:17 -0600

    Taskbar and placeholders for interactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
 import logo from './logo-tran.png';
+import {Button, DropdownButton, Image, Stack, Navbar,Nav, Container, Form } from 'react-bootstrap';
 import './App.css';
-
 function App() {
+  
   return (
     <div className="App">
       
-      <header className="App-header">
-        <Button variant='danger'>Admin</Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tick-it app
-        </p>
-        
-      </header>
+      <Navbar bg='dark' variant='dark' style={{maxHeight:'50px'}} sticky='top'>
+        <Container>
+          <Navbar.Brand href="#home" >TickIt</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Season Pass</Nav.Link>
+            <Nav.Link href="#pricing">Admin Login</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='App-body'>
+        <Stack direction='vertical' style={{alignItems:'center'}} gap={1}>
+          <Image src={logo} className='App-logo-big' style={{marginTop:'-60px'}}></Image>
+          <div style={{borderColor:'black'}}>
+            <Form.Control
+            placeholder="Enter to search for event"
+            id="searchInput"
+            style={{marginTop:'-70px', width:'50vmin'}}
+            />
+          </div> 
+          <div>
+            <p style={{color:'white'}}>
+              show events here
+            </p>
+          </div> 
+        </Stack>
+      </div>
     </div>
   );
 }
