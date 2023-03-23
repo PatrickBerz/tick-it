10b95d0b4d5841ee2a2ddd68367ece78971733e2
PatrickB <patrickberzins14@gmail.com>
2023-03-23 17:08:47 -0500

    console logs password input

--- frontend/src/components/adminLogin.tsx
+++ frontend/src/components/adminLogin.tsx
@@ -1,36 +1,40 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Stack,Button } from 'react-bootstrap';
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import App from '../App';
 
 export const AdminLogin = () =>{
-    useEffect( ()=> {
-        fetchItems();
-    },[]);
 
-    const [items,setItems] = useState([]);
-
-    const fetchItems = async() => {
-        const data = await fetch('/password');
-        const items = await data.json();
-        setItems(items);
+    const [value, setValue] = useState('');
+    
+    const onFormSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(value);
+        setValue(value);
+    }
+    const fetchData = async () => {
+        const {data} = await App.get()
+        console.log(data);
     }
 
     return (
         <Stack direction='vertical' style={{alignItems:'center'}} gap={1}>
             <div className='password-field' style={{marginTop:'150px'}}>
-                <Form.Control
-                type='password'
-                id='passwordField'
-                name='passInput'
-                // value={''}
-                placeholder='Password'
-                >
-                </Form.Control>
-                <Button variant="primary" type="submit" style={{marginTop:'10px'}}>
+                <Form onSubmit={onFormSubmit}>
+                    <Form.Control
+                        type='password'
+                        placeholder='Password'
+                        onChange={(e) => setValue(e.target.value)}
+                        value = {value}
+                    >
+                    </Form.Control>
+                    <Button variant="primary" type="submit" style={{marginTop:'10px'}}>
                     Submit
-                </Button>
+                    </Button>
+                </Form>
+                
             </div>
         </Stack>
     )
