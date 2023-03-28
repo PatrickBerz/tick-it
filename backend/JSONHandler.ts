d20b9930efdd0dbc7d48f9c759c74c2786a85151
Van H <sgrace2701@gmail.com>
2023-03-28 16:09:05 -0500

    Update JSONHandler
    Added try-catch block to deserializer

--- backend/JSONHandler.ts
+++ backend/JSONHandler.ts
@@ -1,6 +1,8 @@
 import * as fs from 'fs';
 
-function serialize<T>(objects: Set<T>, destinationFileName: string) { //Turn a set of objects and turn them into a json file, stored at the given destinationFileName.
+
+
+function serialize<T>(objects: Set<T>, destinationFileName: string): void { //Turn a set of objects and turn them into a json file, stored at the given destinationFileName.
     fs.writeFile(destinationFileName, '{ "objects": [', function(err) {if (err) console.error(err)});
     for (let object of objects) {
         const v = JSON.stringify(object);
@@ -10,13 +12,20 @@ function serialize<T>(objects: Set<T>, destinationFileName: string) { //Turn a s
     fs.writeFile(destinationFileName, ']}', {flag: 'a'}, function(err) {if (err) console.error(err)});
 }
 
-function deserialize<T>(fileName: string) { //Take the name of a json file, turn it into a set of objects and return that set.
-    const objects = JSON.parse(fs.readFileSync(fileName, 'utf-8'));
+function deserialize<T>(fileName: string): Set<T> { //Take the name of a json file, turn it into a set of objects and return that set.
+    
     var collection = new Set<T>();
-    for (let object of objects) {
-        const v = object as T;
-        collection.add(v);
-    }
+    try {
+        const data = fs.readFileSync(fileName, 'utf8');
+        console.log(data);
+        const objects = JSON.parse(data);
+        for (let object of objects) {
+            const v = object as T;
+            collection.add(v);
+        }
+      } catch (err) {
+        console.error(err);
+      }
     return collection;
 }
 
@@ -25,3 +34,4 @@ function deserialize<T>(fileName: string) { //Take the name of a json file, turn
 //function deserializeVenue(fileName: String){} - deserialize<SeatSection>
 //function deserializePurchases(fileName: String){} - deserialize<Purchase>
 //function deserializeSeasonHolders(fileName: String){} - deserialize<SeasonTicketHolder>
+
