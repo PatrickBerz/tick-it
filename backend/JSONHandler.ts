4d14c180f933e17c1214a55d7b1fbe904ece67e3
csmith5084 <cns0030@uah.edu>
2023-03-28 13:04:50 -0500

    Create JSONHandler.ts
    TODO: Figure out how to get the necessary modules imported properly

---
+++ backend/JSONHandler.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+
+function serialize<T>(objects: Set<T>, destinationFileName: String): void { //Turn a set of objects and turn them into a json file, stored at the given destinationFileName.
+    fs.writeFile(destinationFileName, '{ "objects": [', function(err) {if (err) console.error(err)});
+    for (let object of objects) {
+        const v = JSON.stringify(object);
+        fs.writeFile(destinationFileName, v, {flag: 'a'}, function(err) {if (err) console.error(err)});
+        fs.writeFile(destinationFileName, ',', {flag: 'a'}, function(err) {if (err) console.error(err)});
+    }
+    fs.writeFile(destinationFileName, ']}', {flag: 'a'}, function(err) {if (err) console.error(err)});
+}
+
+function deserialize<T>(fileName: String): Set<T> { //Take the name of a json file, turn it into a set of objects and return that set.
+    const objects = JSON.parse(fs.readFile(fileName, 'utf8', function(err) {if (err) console.error(err)}));
+    var collection = new Set<T>();
+    for (let object of objects) {
+        const v = object as T;
+        collection.add(v);
+    }
+    return collection;
+}
+
+//function serializePurchases(purchases: Collections.Set<Purchase>){} - serialize<Purchase>
+//function serializeShows(shows: Collections.Set<Show>){} - serialize<Show>
+//function deserializeVenue(fileName: String){} - deserialize<SeatSection>
+//function deserializePurchases(fileName: String){} - deserialize<Purchase>
+//function deserializeSeasonHolders(fileName: String){} - deserialize<SeasonTicketHolder>
