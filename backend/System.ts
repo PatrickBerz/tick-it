55b09c13aaa74bc3d15ad8495958d908f11bf47d
jayde225 <jayde225@users.noreply.github.com>
2023-03-30 16:14:05 -0500

    Integration of JSON library

--- backend/System.ts
+++ backend/System.ts
@@ -1,18 +1,23 @@
-import { serialize } from "./JSONHandler"
-import { deserialize } from "./JSONHandler"
+//import { serialize } from "./JSONHandler"
+//import { deserialize } from "./JSONHandler"
+import { JsonSerializer, throwError } from 'typescript-json-serializer';
 import { Seat } from "../frontend/src/Seat"
 
 export class System {
 
     constructor() {
+
+        // Instantiate a default serializer
+        const defaultSerializer = new JsonSerializer();
+
         let obj: Seat = new Seat("Orchestra", "B", 12, false, false, 29.99);
         let coll: Set<Seat> = new Set<Seat>();
         coll.add(obj);
-        serialize(coll, "test1.json");
+        let data: string = defaultSerializer.serialize(obj) as unknown as string;
 
         console.log("Serialized obj " + JSON.stringify(obj));
 
-        let set: Set<Seat> = deserialize("test1.json");
+        const set = defaultSerializer.deserializeObject(data, Seat);
         console.log("\n\n\n\n");
         console.log("Deserialized: " + JSON.stringify(set));
     }
