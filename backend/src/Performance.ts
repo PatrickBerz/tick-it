7184c5311b821b35cf9d92fd95596a0559320242
Van H <sgrace2701@gmail.com>
2023-04-11 15:49:40 -0500

    A fix to the JSON Deserializer updates

--- backend/src/Performance.ts
+++ backend/src/Performance.ts
@@ -4,36 +4,45 @@ import { TicketStatus } from "./Ticket"; //Import TicketStatus enum
 import { Venue } from "./Venue";
 
 export class Performance {
-
-    private show: Show;
+    private performanceName: string;
+    private venueName: string;
     private dateTime: Date;
     private tickets: Ticket[];
 
-    constructor(show: Show, dateTime: Date) {
-        
-        this.show = show;
+    //Used to construct a new Performance
+    constructor(performanceName: string, venueName: string, dateTime: Date) {
+        this.performanceName = performanceName;
+        this.venueName = venueName;
         this.dateTime = dateTime;
-
-        this.tickets = this.makeTickets(this.show.getVenue())
+        this.tickets = [];
     }
 
-    getShow() { return this.show}
+    //Return the name of a performance
+    getPerformanceName() { return this.performanceName };
 
+    //Return the name of the venue
+    getVenueName() { return this.venueName };
+
+    //Return the date and time
     getDateTime() { return this.dateTime}
 
+    //Return the collection of tickets
     getTickets() { return this.tickets}
 
-    private makeTickets(venue:Venue) {
-        
-        let tickets:Ticket[] = [];
+    //Change the collection of tickets
+    setTickets(tickets: Ticket[]) { this.tickets = tickets };
 
-        venue.getSections().forEach(section => {
+    //Generate a collection of tickets based on the Venue object
+    private makeTickets(venue:Venue) {
+      let tickets:Ticket[] = [];
+
+       venue.getSections().forEach(section => {
             section.getSeats().forEach(seat => {
-                let newTicket:Ticket = new Ticket(this, seat);
+                let newTicket:Ticket = new Ticket(this.performanceName, seat);
                 tickets.push(newTicket);
             });
         });
 
-        return tickets;
+        this.tickets = tickets;
     }
 }
--- backend/src/Venue.ts
+++ backend/src/Venue.ts
@@ -3,9 +3,11 @@ import { SeatSection } from "./SeatSection"; //Import SeatSection class
 export class Venue {
     private seatSections: SeatSection[];
 
+    //Construct a new Venue
     constructor(seatSections: SeatSection[]) {
         this.seatSections = seatSections;
     }
 
+    //Return the collection of seat sections in the Venue
     getSections() { return this.seatSections; }
 }
