eb34f0b9a1e54116849d33b83b22008d9492dc48
PatrickB <patrickberzins14@gmail.com>
2023-04-11 16:28:53 -0500

    broken

--- backend/routes/handler.ts
+++ backend/routes/handler.ts
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const cors = require("cors")
+const cors = require("cors");
+import { JSONHandler } from "../JSONHandler"; 
 import { Seat } from "../src/Seat";
+import { Ticket } from "../src/Ticket";
+import { Purchase } from "../src/Purchase";
 
 router.use(cors({
     origin: '*'
@@ -9,16 +12,13 @@ router.use(cors({
 
 router.get("/ticketData", (req, res) => {
 
-    let seat:Seat = new Seat("Orchestra", "A", 12, false, false, 15.99)
-    // const ticket = [{
-    //     "performance": "Phantom of the Opera",
-    //     "seat": "A12",
-    //     "ticketStatus": "Reserved",
-    //     "price": "15.99"
-    // }];
-    const data = { name: "John Doe", age: 30}
-    //res.json(JSON.stringify(seat));
-    res.json(data);
+    //let seat = new Seat("Orchestra", "A", 12, false, false, 15.99)
+    // let jsonhandler = new JSONHandler()
+    
+    // jsonhandler.deserializePurchase('backend/test6.json')
+    // let ticket: any[] = jsonhandler.getData() 
+    // //const ticket = '[{"purchaser":{"name":"Susan","address":"123 Sesame Street","phoneNum":"6064135244"},"confNum":0,"tickets":[{"performance":"West Side Story","seat":{"section":"Orchestra","row":"B","seatNum":12,"acessible":false,"inSeasonSection":false,"defaultPrice":29.99},"ticketStatus":0,"price":29.99}]}]'
+    // res.json(ticket);
 });
 router.post("/password", (req, res) => {
     //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000/password');
--- frontend/src/components/ticketStuff.jsx
+++ frontend/src/components/ticketStuff.jsx
@@ -1,13 +1,27 @@
 import { Form, Stack,Button, Alert } from 'react-bootstrap';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+
 
 export const TicketStuff = () =>{
-    
+    const [data,setData] = useState({});
 
+    useEffect(() => {
+        fetch('http://localhost:4000/ticketData')
+        .then(res => res.json())
+        .then(data => setData(data))
+        .catch(err => console.log(err))
+    }, []);
+    
     return (
-        <Stack direction='vertical' style={{color: 'white', alignItems:'center', marginTop:'80px' }} gap={1}>
-                <div style={{fontSize:'30px'}}>Tickets Page</div>
-                
-        </Stack>
+        <div style={{fontSize:'30px', color:"white"}}>
+           <h1>Ticket Records</h1>
+           <ul>
+            {data.map((item,index)=>(
+                <li key={index}>
+                <p> Performance: {item.name}</p>
+                </li>
+            ))}
+           </ul>
+        </div>
     )
 }
