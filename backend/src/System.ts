a68a28a7318a901ca8d07cf9e4cfda6dc7f3211e
Nathaniel Smith <cns0030@uah.edu>
2023-04-11 15:56:47 -0500

    Reimplementing System class prototype.

--- backend/src/System.ts
+++ backend/src/System.ts
@@ -1,4 +1,29 @@
-class System {
+import { Seat } from "./Seat";
+import { Ticket } from "./Ticket";
+import { Performance } from "./Performance";
+import { SeatSection } from "./SeatSection";
+import { Attendee } from "./Attendee";
+import { Venue } from "./Venue";
+import { SeasonTicketHolder } from "./SeasonTicketHolder";
+import { Show } from "./Show";
+import { Purchase } from "./Purchase";
+export class System {
+    private venues : Venue[] = [];
+    private shows : Show[] = [];
+    private purchases : Purchase[] = [];
+    private seasonTicketHolders : SeasonTicketHolder[] = [];
 
-    
+    constructor(filePath: string) {
+        this.venues = this.initializeVenues(filePath);
+        this.shows = this.initializeShows(filePath);
+        this.purchases = this.initializePurchases(filePath);
+        this.seasonTicketHolders = this.initializeSeasonHolders(filePath);
+    }
+
+    private initializeVenues(filePath : string) : Venue[] {return []} //temporary
+    private initializeShows(filePath : string) : Show[] {return []}
+    private initializePurchases(filePath : string) : Purchase[] {return []}
+    private initializeSeasonHolders(filePath : string) : SeasonTicketHolder[] {return []}
+
+    createPurchase(){}
 }
