f6a0fea7eec2e8a83f9414365f2fac759383b09c
PatrickB <patrickberzins14@gmail.com>
2023-04-11 15:03:08 -0500

    season pass seat selection and checkout page navigation
    added season pass seat selection and checkout page navigation

--- frontend/src/components/navigator.jsx
+++ frontend/src/components/navigator.jsx
@@ -12,7 +12,7 @@ export default class Navigator extends Component{
                     <Navbar.Brand as={Link} to={"/"} style={{color:'#FF4057'}} >TickIt</Navbar.Brand>
                         <Nav className="me-auto">
                             <Nav.Link as={Link} to={"/"} >Home</Nav.Link>
-                            <Nav.Link as={Link} to={"/seasonPass"} >Season Pass</Nav.Link>
+                            <Nav.Link as={Link} to={"/seasonVenueSelection"} >Season Pass</Nav.Link>
                             <Nav.Link as={Link} to={"/adminLogin"} >Admin Login</Nav.Link>
 
                         </Nav>
