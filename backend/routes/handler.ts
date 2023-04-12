40d7ce8c64f8922b40d6ec7844c5ae11e1527b04
PatrickB <patrickberzins14@gmail.com>
2023-04-11 23:18:11 -0500

    sends example json to front end

--- backend/routes/handler.ts
+++ backend/routes/handler.ts
@@ -14,30 +14,99 @@ router.get("/ticketData", (req, res) => {
 
     //let seat = new Seat("Orchestra", "A", 12, false, false, 15.99)
     // let jsonhandler = new JSONHandler()
-    
+
     // jsonhandler.deserializePurchase('backend/test6.json')
     // let ticket: any[] = jsonhandler.getData() 
     // //const ticket = '[{"purchaser":{"name":"Susan","address":"123 Sesame Street","phoneNum":"6064135244"},"confNum":0,"tickets":[{"performance":"West Side Story","seat":{"section":"Orchestra","row":"B","seatNum":12,"acessible":false,"inSeasonSection":false,"defaultPrice":29.99},"ticketStatus":0,"price":29.99}]}]'
     // res.json(ticket);
-     // const ticket = [{
-    //     "performance": "Phantom of the Opera",
-    //     "seat": "A12",
-    //     "ticketStatus": "Reserved",
-    //     "price": "15.99"
-    // }];
-    const data = [{"purchaser":{"name":"Susan","address":"123 Sesame Street","phoneNum":"6064135244"},"confNum":0,"tickets":[{"performance":"West Side Story","seat":{"section":"Orchestra","row":"B","seatNum":12,"acessible":false,"inSeasonSection":false,"defaultPrice":29.99},"ticketStatus":0,"price":29.99}]}]
+    const ticket = [
+        {
+            "ticket":
+            {
+                "confNum": 0,
+                "performance": "West Side Story",
+                "seat":
+                {
+                    "section": "Orchestra",
+                    "row": "B",
+                    "seatNum": 12,
+                    "acessible": false,
+                    "inSeasonSection": false,
+                    "defaultPrice": 29.99
+                },
+                "ticketStatus": 0,
+                "price": 29.99,
+                "purchaser":
+                {
+                    "name": "Susan",
+                    "address": "123 Sesame Street",
+                    "phoneNum": "6064135244"
+                },
+            },
 
-    //res.json(JSON.stringify(seat));
-    //res.json(JSON.stringify(data));
+        },
+        {
+            "ticket":
+            {
+                "confNum": 1,
+                "performance": "West Side Story",
+                "seat":
+                {
+                    "section": "Orchestra",
+                    "row": "B",
+                    "seatNum": 13,
+                    "acessible": false,
+                    "inSeasonSection": false,
+                    "defaultPrice": 29.99
+                },
+                "ticketStatus": 0,
+                "price": 29.99,
+                "purchaser":
+                {
+                    "name": "Susan",
+                    "address": "123 Sesame Street",
+                    "phoneNum": "6064135244"
+                },
+            },
+
+        },
+        {
+            "ticket":
+            {
+                "confNum": 3,
+                "performance": "Magic Mike",
+                "seat":
+                {
+                    "section": "Orchestra",
+                    "row": "C",
+                    "seatNum": 34,
+                    "acessible": false,
+                    "inSeasonSection": false,
+                    "defaultPrice": 29.99
+                },
+                "ticketStatus": 0,
+                "price": 29.99,
+                "purchaser":
+                {
+                    "name": "Tom",
+                    "address": "Easy Street",
+                    "phoneNum": "6064135244"
+                },
+            },
+
+        }
+    ]
+    res.json(ticket);
 });
+
 router.post("/password", (req, res) => {
     //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000/password');
     let password = req.body.value;
     console.log(password);
-    if (password == 'admin'){
+    if (password == 'admin') {
         res.status(200);
     }
-    else{
+    else {
         res.status(418);
     }
     // const str = [{
--- frontend/src/components/ticketStuff.jsx
+++ frontend/src/components/ticketStuff.jsx
@@ -3,15 +3,24 @@ import React, { useState, useEffect } from 'react';
 
 
 export const TicketStuff = () => {
-    // const [data,setData] = useState({});
+    const [data, setData] = useState([]);
 
-    // useEffect(() => {
-    //     fetch('http://localhost:4000/ticketData')
-    //     .then((res) => res.json())
-    //     .then(() => setData(data))
-    //     .catch(err => console.log(err))
-    // }, []);
-
+    const fetchData = () => {
+      fetch(`http://localhost:4000/ticketData`)
+        .then((response) => response.json())
+        .then((data) => {
+          //console.log(data);
+          setData(data);
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    };
+    useEffect(() => {
+      fetchData();
+    }, []);
+    
     // let data = [
     //     {
     //         "purchaser":
@@ -87,89 +96,89 @@ export const TicketStuff = () => {
     //     }
     // ]
 
-    let data = [
-        {
-            "ticket":
-            {
-                "confNum": 0,
-                "performance": "West Side Story",
-                "seat":
-                {
-                    "section": "Orchestra",
-                    "row": "B",
-                    "seatNum": 12,
-                    "acessible": false,
-                    "inSeasonSection": false,
-                    "defaultPrice": 29.99
-                },
-                "ticketStatus": 0,
-                "price": 29.99,
-                "purchaser":
-                {
-                    "name": "Susan",
-                    "address": "123 Sesame Street",
-                    "phoneNum": "6064135244"
-                },
-            },
+    // let data = [
+    //     {
+    //         "ticket":
+    //         {
+    //             "confNum": 0,
+    //             "performance": "West Side Story",
+    //             "seat":
+    //             {
+    //                 "section": "Orchestra",
+    //                 "row": "B",
+    //                 "seatNum": 12,
+    //                 "acessible": false,
+    //                 "inSeasonSection": false,
+    //                 "defaultPrice": 29.99
+    //             },
+    //             "ticketStatus": 0,
+    //             "price": 29.99,
+    //             "purchaser":
+    //             {
+    //                 "name": "Susan",
+    //                 "address": "123 Sesame Street",
+    //                 "phoneNum": "6064135244"
+    //             },
+    //         },
             
-        },
-        {
-            "ticket":
-            {
-                "confNum": 1,
-                "performance": "West Side Story",
-                "seat":
-                {
-                    "section": "Orchestra",
-                    "row": "B",
-                    "seatNum": 13,
-                    "acessible": false,
-                    "inSeasonSection": false,
-                    "defaultPrice": 29.99
-                },
-                "ticketStatus": 0,
-                "price": 29.99,
-                "purchaser":
-                {
-                    "name": "Susan",
-                    "address": "123 Sesame Street",
-                    "phoneNum": "6064135244"
-                },
-            },
+    //     },
+    //     {
+    //         "ticket":
+    //         {
+    //             "confNum": 1,
+    //             "performance": "West Side Story",
+    //             "seat":
+    //             {
+    //                 "section": "Orchestra",
+    //                 "row": "B",
+    //                 "seatNum": 13,
+    //                 "acessible": false,
+    //                 "inSeasonSection": false,
+    //                 "defaultPrice": 29.99
+    //             },
+    //             "ticketStatus": 0,
+    //             "price": 29.99,
+    //             "purchaser":
+    //             {
+    //                 "name": "Susan",
+    //                 "address": "123 Sesame Street",
+    //                 "phoneNum": "6064135244"
+    //             },
+    //         },
             
-        },
-        {
-            "ticket":
-            {
-                "confNum": 3,
-                "performance": "Magic Mike",
-                "seat":
-                {
-                    "section": "Orchestra",
-                    "row": "C",
-                    "seatNum": 34,
-                    "acessible": false,
-                    "inSeasonSection": false,
-                    "defaultPrice": 29.99
-                },
-                "ticketStatus": 0,
-                "price": 29.99,
-                "purchaser":
-                {
-                    "name": "Tom",
-                    "address": "Easy Street",
-                    "phoneNum": "6064135244"
-                },
-            },
+    //     },
+    //     {
+    //         "ticket":
+    //         {
+    //             "confNum": 3,
+    //             "performance": "Magic Mike",
+    //             "seat":
+    //             {
+    //                 "section": "Orchestra",
+    //                 "row": "C",
+    //                 "seatNum": 34,
+    //                 "acessible": false,
+    //                 "inSeasonSection": false,
+    //                 "defaultPrice": 29.99
+    //             },
+    //             "ticketStatus": 0,
+    //             "price": 29.99,
+    //             "purchaser":
+    //             {
+    //                 "name": "Tom",
+    //                 "address": "Easy Street",
+    //                 "phoneNum": "6064135244"
+    //             },
+    //         },
             
-        }
-    ]
+    //     }
+    // ]
 
     return (
         <div >
             <h1 style={{ fontSize: '30px', color: "white" }}>Ticket Records</h1>
             <Table responsive striped hover variant='dark' size='sm'>
-                <tbody style={{ fontSize: '20px', color: "white" }}>
+                <tbody style={{ fontSize: '20px', color: "white"}}>
                     <tr>
                         <th>Confirmation #</th>
                         <th>Purchaser</th>
@@ -177,8 +186,9 @@ export const TicketStuff = () => {
                         <th>Seat(s)</th>
                         <th>paid?</th>
                     </tr>
-                    {data.map((item, index) => (
-                        <tr key={index}>
+                    
+                        {data.map((item, index) => (
+                        <tr key={index} style={{alignItems:'center'}}>
                             <td>{item.ticket.confNum}</td>
                             <td>{item.ticket.purchaser.name}</td>
                             <td>{item.ticket.performance}</td>
@@ -186,6 +196,7 @@ export const TicketStuff = () => {
                             <td>{item.ticket.ticketStatus}</td>
                         </tr>
                     ))}
+                    
                 </tbody>
             </Table>
 
