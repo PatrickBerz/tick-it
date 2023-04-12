7a8774312e432253987ec69b20d56d6f9a0c4286
PatrickB <patrickberzins14@gmail.com>
2023-04-11 20:45:55 -0500

    Tickets are main object, buyer held in ticket info

--- frontend/src/components/ticketStuff.jsx
+++ frontend/src/components/ticketStuff.jsx
@@ -1,8 +1,8 @@
-import { Form, Stack,Button, Alert, Table } from 'react-bootstrap';
-import React, {useState, useEffect} from 'react';
+import { Form, Stack, Button, Alert, Table } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
 
 
-export const TicketStuff = () =>{
+export const TicketStuff = () => {
     // const [data,setData] = useState({});
 
     // useEffect(() => {
@@ -11,61 +11,165 @@ export const TicketStuff = () =>{
     //     .then(() => setData(data))
     //     .catch(err => console.log(err))
     // }, []);
-    
+
+    // let data = [
+    //     {
+    //         "purchaser":
+    //         {
+    //             "name": "Susan",
+    //             "address": "123 Sesame Street",
+    //             "phoneNum": "6064135244"
+    //         },
+    //         "confNum": 0,
+    //         "tickets":
+    //         {
+    //             "performance": "West Side Story",
+    //             "seat":
+    //             {
+    //                 "section": "Orchestra",
+    //                 "row": "B",
+    //                 "seatNum": 12,
+    //                 "acessible": false,
+    //                 "inSeasonSection": false,
+    //                 "defaultPrice": 29.99
+    //             },
+    //             "ticketStatus": 0,
+    //             "price": 29.99
+    //         }
+    //     },
+    //     {
+    //         "purchaser":
+    //         {
+    //             "name": "Susan",
+    //             "address": "123 Sesame Street",
+    //             "phoneNum": "6064135244"
+    //         },
+    //         "confNum": 1,
+    //         "tickets":
+    //         {
+    //             "performance": "West Side Story",
+    //             "seat":
+    //             {
+    //                 "section": "Orchestra",
+    //                 "row": "B",
+    //                 "seatNum": 13,
+    //                 "acessible": false,
+    //                 "inSeasonSection": false,
+    //                 "defaultPrice": 29.99
+    //             },
+    //             "ticketStatus": 0,
+    //             "price": 29.99
+    //         }
+    //     },
+    //     {
+    //         "purchaser":
+    //         {
+    //             "name": "Hugh Janus",
+    //             "address": "Easy street",
+    //             "phoneNum": "6064135244"
+    //         },
+    //         "confNum": 2,
+    //         "tickets":
+    //         {
+    //             "performance": "The Last Airbender",
+    //             "seat":
+    //             {
+    //                 "section": "Orchestra",
+    //                 "row": "B",
+    //                 "seatNum": 24,
+    //                 "acessible": false,
+    //                 "inSeasonSection": false,
+    //                 "defaultPrice": 29.99
+    //             },
+    //             "ticketStatus": 0,
+    //             "price": 29.99
+    //         }
+    //     }
+    // ]
+
     let data = [
-        {"purchaser":
+        {
+            "ticket":
             {
-                "name":"Susan",
-            "address":"123 Sesame Street",
-            "phoneNum":"6064135244"
-            },
-            "confNum":0,
-            "tickets":
-                {
-                    "performance":"West Side Story",
+                "confNum": 0,
+                "performance": "West Side Story",
                 "seat":
                 {
-                    "section":"Orchestra",
-                "row":"B",
-                "seatNum":12,
-                "acessible":false,
-                "inSeasonSection":false,
-                "defaultPrice":29.99},
-                "ticketStatus":0,
-                "price":29.99
-            }
+                    "section": "Orchestra",
+                    "row": "B",
+                    "seatNum": 12,
+                    "acessible": false,
+                    "inSeasonSection": false,
+                    "defaultPrice": 29.99
+                },
+                "ticketStatus": 0,
+                "price": 29.99,
+                "purchaser":
+                {
+                    "name": "Susan",
+                    "address": "123 Sesame Street",
+                    "phoneNum": "6064135244"
+                },
+            },
+            
         },
         {
-            "purchaser":
+            "ticket":
             {
-                "name":"Hugh Janus",
-                "address":"Easy street",
-                "phoneNum":"6064135244"
-            },
-            "confNum":1,
-            "tickets":
-            {
-                "performance":"The Last Airbender",
+                "confNum": 1,
+                "performance": "West Side Story",
                 "seat":
                 {
-                    "section":"Orchestra",
-                    "row":"B",
-                    "seatNum":24,
-                    "acessible":false,
-                    "inSeasonSection":false,
-                    "defaultPrice":29.99},
-                    "ticketStatus":0,
-                    "price":29.99
-                }
-            }
-        ]
-
+                    "section": "Orchestra",
+                    "row": "B",
+                    "seatNum": 13,
+                    "acessible": false,
+                    "inSeasonSection": false,
+                    "defaultPrice": 29.99
+                },
+                "ticketStatus": 0,
+                "price": 29.99,
+                "purchaser":
+                {
+                    "name": "Susan",
+                    "address": "123 Sesame Street",
+                    "phoneNum": "6064135244"
+                },
+            },
+            
+        },
+        {
+            "ticket":
+            {
+                "confNum": 3,
+                "performance": "Magic Mike",
+                "seat":
+                {
+                    "section": "Orchestra",
+                    "row": "C",
+                    "seatNum": 34,
+                    "acessible": false,
+                    "inSeasonSection": false,
+                    "defaultPrice": 29.99
+                },
+                "ticketStatus": 0,
+                "price": 29.99,
+                "purchaser":
+                {
+                    "name": "Tom",
+                    "address": "Easy Street",
+                    "phoneNum": "6064135244"
+                },
+            },
+            
+        }
+    ]
 
     return (
         <div >
-           <h1 style={{fontSize:'30px', color:"white"}}>Ticket Records</h1>
+            <h1 style={{ fontSize: '30px', color: "white" }}>Ticket Records</h1>
             <Table responsive striped hover variant='dark' size='sm'>
-                <tbody style={{fontSize:'20px', color:"white"}}>
+                <tbody style={{ fontSize: '20px', color: "white" }}>
                     <tr>
                         <th>Confirmation #</th>
                         <th>Purchaser</th>
@@ -75,11 +179,11 @@ export const TicketStuff = () =>{
                     </tr>
                     {data.map((item, index) => (
                         <tr key={index}>
-                            <td>{item.confNum}</td>
-                            <td>{item.purchaser.name}</td>
-                            <td>{item.tickets.performance}</td>
-                            <td>{item.tickets.seat.seatNum}</td>
-                            <td>{item.tickets.ticketStatus}</td>
+                            <td>{item.ticket.confNum}</td>
+                            <td>{item.ticket.purchaser.name}</td>
+                            <td>{item.ticket.performance}</td>
+                            <td>{item.ticket.seat.seatNum}</td>
+                            <td>{item.ticket.ticketStatus}</td>
                         </tr>
                     ))}
                 </tbody>
@@ -100,7 +204,7 @@ export const TicketStuff = () =>{
                 ))}
             </tbody>
            </Table> */}
-           {/* <ul>
+            {/* <ul>
             {data.map((item,index)=>(
                 <li key={index}>
                 <p> Performance: {item.name}</p>
