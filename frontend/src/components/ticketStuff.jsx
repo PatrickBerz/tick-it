b3f41e54f5a18232a500973caed4a1a14e32c3cd
bal0015 <bal0015@uah.edu>
2023-04-11 22:27:26 -0500

    Testing frontend
    I have no clue how frontend works, just warning you

--- frontend/src/components/ticketStuff.jsx
+++ frontend/src/components/ticketStuff.jsx
@@ -5,23 +5,29 @@ import React, {useState, useEffect} from 'react';
 export const TicketStuff = () =>{
     const [data,setData] = useState({});
 
+    
+
     useEffect(() => {
         fetch('http://localhost:4000/ticketData')
         .then(res => res.json())
         .then(data => setData(data))
         .catch(err => console.log(err))
     }, []);
+
+    //console.log(res)
     
     return (
         <div style={{fontSize:'30px', color:"white"}}>
-           <h1>Ticket Records</h1>
-           <ul>
-            {data.map((item,index)=>(
-                <li key={index}>
-                <p> Performance: {item.name}</p>
-                </li>
-            ))}
-           </ul>
+           <h1>Ticket Records</h1> {
+                JSON.stringify(data[0]["purchaser"])
+                // data.map((data) => ( 
+                //     <ol key = { data } >
+                //         User_Name: { data.username }, 
+                //         Full_Name: { data.name }, 
+                //         User_Email: { data.email } 
+                //         </ol>
+                // ))
+            }
         </div>
     )
 }
