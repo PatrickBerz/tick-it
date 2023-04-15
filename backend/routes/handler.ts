621e1b4d2b5c10a8aa6a12827a5936ae9151ad2c
bal0015 <bal0015@uah.edu>
2023-04-15 00:48:47 -0500

    Update handler.ts
    More stuff added, none tested but really just skeletons and comments to know what to do once System functions are known

--- backend/routes/handler.ts
+++ backend/routes/handler.ts
@@ -8,12 +8,22 @@ import cors from "cors"
 import { JSONHandler } from "../JSONHandler"; 
 import { Seat } from "../src/Seat";
 import { SeasonTicketHolder } from "../src/SeasonTicketHolder";
-
+import { Venue } from "../src/Venue";
+import { SeatSection } from "../src/SeatSection";
+import { Purchase } from "../src/Purchase";
+import { Attendee } from "../src/Attendee";
+import { ConfNum } from "../src/ConfNum"
+import { Ticket } from "../src/Ticket";
+import { Performance } from "../src/Performance";
 
 //FUNCTIONS NEEDED:
 //
 // Lookup performance by showName, venue, dateTime
 // Lookup purchase by confNum
+// Add purchase to list
+// Add performance to list
+// Add season ticket holder to list
+// Basically, add everything to lists
 
 
 router.use(cors({
@@ -36,7 +46,8 @@ router.use(cors({
 router.get("/seasonTickets"), (req, res) => {
     //get list of season ticket holders from System
     let seasonTixList: SeasonTicketHolder[] = [ new SeasonTicketHolder("Richard Blargson", "5000 Fancy Boulevard", "123-555-5555", new Seat("Orchestra", "A", 15, false, true, 99999.99)),
-                                                new SeasonTicketHolder("Moneyton Blargson", "5001 Fancy Boulevard", "123-555-5556", new Seat("Orchestra", "A", 16, false, true, 99999.99))]
+                                                new SeasonTicketHolder("Moneyton Blargson", "5001 Fancy Boulevard", "123-555-5556", new Seat("Orchestra", "A", 16, false, true, 99999.99))] 
+    // let seasonTixList: SeasonTicketHolder[] = System.getSeasonHolders()
     res.json(seasonTixList)
 }
 
@@ -54,8 +65,33 @@ router.post("/newSeasonTicket"), (req, res) => {
 }
 
 router.post("/newDefaults"), (req, res) => {
+
     // parse defaults. Need venue name and assoc. array of section name to new default price
-    // call Sys function to replace old venue? It would have to use JSONHandler to serialize
+    // {
+    //     "venueName": "Playhouse",
+    //     "sections": [
+    //                  "Section1": 23.99,
+    //                  "Section2": 33.99,
+    //                  "Section3": 23.99
+    //                  ]
+    // }
+    let data = req.body
+
+    //call Sys class to get old venue of same name
+    //Replace declaration with Sys function call
+    let venue:Venue = new Venue();
+
+    let sectionList = venue.getSections()
+    data.sections.forEach(sectionName => {
+        sectionList.forEach((section) => {
+            if (section.getSectionNum() == sectionName) {
+                section.setDefaultPrice(data.sections[sectionName])
+            }
+        });
+    });
+
+    //no need to call Sys function since returned venue should be a reference to the one in the list
+    //definitely worth testing that to make sure
     
 }
 
@@ -66,17 +102,24 @@ router.get("/ticketData/:showName/:venue/:dateTime"), (req, res) => {
 }
 
 router.post("/exchange"), (req, res) => {
-    //need old conf num, new showName, new venue, new DateTime, new tickets
+    //need old conf num (already verified hopefully), new showName, new venue, new DateTime, new tickets
+
+    //Sys call to get purchase by confNum
+    //Sys call to get performance by venue, showName, dateTime
+
+    //create new purchase
 }
 
 
-//OLD STUFF, not changed
+
 router.get("/purchaseData", (req, res) => {
+    // TODO: need System function to get list of purchases
 
     let jsonhandler = new JSONHandler()
     
     //jsonhandler.deserializePurchase('../test6.json')
     jsonhandler.deserializePurchase(__dirname + "/../samplePurchases.json")
+
     let purchases: any[] = jsonhandler.getData() 
     console.log(purchases)
     console.log("\n\n")
@@ -85,11 +128,67 @@ router.get("/purchaseData", (req, res) => {
 });
 
 router.post("/newPurchase"), (req, res) => {
-    //need show name, dateTime, ticket list
+    //need venue name, show name, dateTime, ticket list, attendee info, ticketStatus (paid, picked up, or not)
+    let data = req.body
+    // {
+    //     "venueName": "Playhouse",
+    //     "showName": "Oklahoma",
+    //     "dateTime": "2023-04-15T04:33:10+0000"
+    //     "ticketList": [
+    //                     {
+    //                         "sectionName": "Orchestra",
+    //                         "row": "B",
+    //                         "seatNum": "12",
+    //                     }
+    //                    ],
+    //     "attendee": {
+    //                     "name": "Ben",
+    //                     "address": "123 address blvd",
+    //                     "phoneNum": "123-444-1234"
+    //                 }
+    //     "ticketStatus": 1 
+    // }
 
     //TODO: figure out where to make purchase
     //pass all into System, where it makes a new purchase and adds to list?
     //OR, make new purchase here, then pass to System to add to list
+
+    //NOTE: This is SUPER ugly and probably O(N^2)
+
+    let newPurchase: Purchase = new Purchase(new Attendee(data.attendee.name, data.attendee.address, data.attendee.phoneNum))
+    newPurchase.setConfNum(ConfNum.getNum())
+    let newTickets: Ticket[] = [];
+    //Sys call to get performance from showName, venue name, dateTime
+    let perf: Performance = new Performance()
+    data.ticketList.forEach(purchTicket => {
+        perf.getTickets().forEach(perfTicket => {
+            if (purchTicket.sectionName == perfTicket.getSeat().getSection() &&
+                purchTicket.row == perfTicket.getSeat().getRow() && 
+                purchTicket.seatNum == perfTicket.getSeat().getSeatNum()) {
+
+                    newTickets.push(perfTicket)
+                }
+        });
+    });
+
+    //TODO: Error checking?
+    newPurchase.updateTickets(newTickets)
+    switch(data.ticketStatus){
+        case 1: {
+            newPurchase.reservedTickets()
+            break;
+        }
+        case 2: {
+            newPurchase.payTickets()
+            break;
+        }
+        case 3: {
+            newPurchase.pickUpTickets()
+            break;
+        }
+    }
+
+    //Sys call to add purchase to list and reserialize
 }
 
 router.post("/password", (req, res) => {
@@ -102,11 +201,6 @@ router.post("/password", (req, res) => {
     else{
         res.status(418);
     }
-    // const str = [{
-    //     "name": "tick-it",
-    //     "password": "AdminLog"
-    // }];
-    // res.end(JSON.stringify(str));
     res.end();
 });
 
