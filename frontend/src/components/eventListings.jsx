e070111f48de60ed3cfc0d46d69f374e4fc1dba6
PatrickB <patrickberzins14@gmail.com>
2023-04-14 19:37:03 -0500

    create events button
    fill in the event form will add event to table

--- frontend/src/components/eventListings.jsx
+++ frontend/src/components/eventListings.jsx
@@ -1,115 +1,179 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form, Stack,Button, Alert, Table, Modal, FormGroup } from 'react-bootstrap';
-import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Form, Stack, Button, Alert, Table, Modal, FormGroup, Row, Col } from 'react-bootstrap'
+import React, { useState } from 'react'
 
-export const EventListings = () =>{
-    const [showModal, setShow] = useState(false);
-    const [alert, setAlert] = useState(undefined);
-    const [value, setValue] = useState('');
+export const EventListings = () => {
+    const [formData, setFormData] = useState({
+        performance: '',
+        venue: '',
+        date: ''
+    });
 
+    const [showModal, setShow] = useState(false)
+    const [formError, setFormError] = useState(null)
+    const [showData, setShowData] = useState([])
+    const [validated, setValidated] = useState(false)
 
-    const confNumModal = () => {
+    const newEventModal = () => {
         setShow(true)
     };
     const handleClose = () => {
         setShow(false)
-        setAlert(undefined); 
+        //setAlert(undefined)
+        setValidated(false)
     }
+
+    const handleTextChange = e => {
+        setFormData({
+            ...formData,
+            performance: e.target.value
+        })
+    }
+    const handleSelectChange = e => {
+        setFormData({
+            ...formData,
+            venue: e.target.value
+        })
+    }
+    const handleDateChange = e => {
+        setFormData({
+            ...formData,
+            date: e.target.value
+        })
+    }
+
     const onFormSubmit = (e) => {
-        e.preventDefault();
-        setValue(value);
-        console.log(value);
-        
-        const promise = fetch('http://localhost:4000/confNum', {
-            method: 'POST',
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({value})
-        });
-        console.log(promise)
-        promise.then(event => {
-            if (event.status === 200) {
-              setAlert({ label: 'success', type: 'success' })
-              
-            } else {
-              setAlert({ label: `Error ${event.statusText}`, type: 'danger' })
-            }
-          })
+        e.preventDefault()
+        if (!formData.performance || !formData.venue || !formData.date) {
+            setFormError('Please fill in all fields.');
+            return;
         }
+
+        setFormError(null);
+
+        console.log('form data', formData)
+
+        setShowData([
+            ...showData,
+            {
+                show: {
+                    performance: formData.performance,
+                    venue: formData.venue,
+                    date: formData.date
+                }
+            }
+        ])
+
+        setFormData({
+            performance: '',
+            venue: '',
+            date: ''
+        })
+        handleClose()
+
+
+        //console.log('form data', showData.show.performance)
+
+        // handleSelectShow()
+
+        // const promise = fetch('http://localhost:4000/confNum', {
+        //     method: 'POST',
+        //     headers: { "Content-Type": "application/json" },
+        //     body: JSON.stringify({ value })
+        // });
+        // console.log(promise)
+        // promise.then(event => {
+        //     if (event.status === 200) {
+        //         setAlert({ label: 'success', type: 'success' })
+
+        //     } else {
+        //         setAlert({ label: `Error ${event.statusText}`, type: 'danger' })
+        //     }
+        // })
+
+    }
+
 
     // Perf name, venue name, date/time, array of tickets
     return (
-        <div className='border border-light-2' style={{maxWidth:'100%', maxHeight:'100%', alignSelf:'center', marginTop:'60px', paddingLeft:'25px', paddingRight:'25px'}}>
-        <Stack direction='vertical' style={{marginTop:'40px'}} gap={2}>
-            <div >
-                <Button style={{
-                        borderColor: '#FF4057', 
-                        backgroundColor:'#FF4057',
-                        }} 
-                        onClick={confNumModal}>
-                    Create Event
-                </Button>
-            </div>
-            <Table align='center' bordered responsive striped hover variant='dark' size='sm' style={{maxHeight:'70%'}}>
-                <thead><tr><th style={{textAlign:'center', fontSize:'20px'}} colSpan={6}>
-                    Ticket Orders 
+        <div className='border border-light-2' style={{ maxWidth: '100%', maxHeight: '100%', alignSelf: 'center', marginTop: '60px', paddingLeft: '25px', paddingRight: '25px' }}>
+            <Stack direction='vertical' style={{ marginTop: '40px' }} gap={2}>
+                <div >
+                    <Button style={{
+                        borderColor: '#FF4057',
+                        backgroundColor: '#FF4057',
+                    }}
+                        onClick={newEventModal}>
+                        Create Event
+                    </Button>
+                </div>
+                <Table align='center' bordered responsive striped hover variant='dark' size='sm' style={{ maxHeight: '70%' }}>
+                    <thead><tr><th style={{ textAlign: 'center', fontSize: '20px' }} colSpan={6}>
+                        Shows
                     </th>
                     </tr>
-                </thead>
-                    <tbody style={{ fontSize: '20px', color: "white"}}>
-                    <tr>
-                        <th style={{textAlign:'center'}}>Performance</th>
-                        <th style={{textAlign:'center'}}>Venue</th>
-                        <th style={{textAlign:'center'}}>Date</th>
-                        <th style={{textAlign:'center'}}>Seats Left</th>
-                    </tr>
-                    
-                        {/* {data.map((item, index) => (
-                        <tr key={index} style={{alignItems:'center'}}>
-                            <td style={{textAlign:'center'}}>{item.ticket.confNum}</td>
-                            <td>{item.ticket.purchaser.first} </td>
-                            <td>{item.ticket.purchaser.last} </td>
-                            <td>{item.ticket.performance}</td>
-                            <td style={{textAlign:'center'}}>{item.ticket.seat.seatNum}</td>
-                            <td style={{textAlign:'center'}}>{item.ticket.paymentStatus}</td>
+                    </thead>
+                    <tbody style={{ fontSize: '20px', color: "white" }}>
+                        <tr>
+                            <th style={{ textAlign: 'center' }}>Performance</th>
+                            <th style={{ textAlign: 'center' }}>Venue</th>
+                            <th style={{ textAlign: 'center' }}>Date</th>
+                            <th style={{ textAlign: 'center' }}>Seats Left</th>
                         </tr>
-                    ))} */}
-                    
-                </tbody>
-            </Table>
-       </Stack>
-       <Modal show={showModal} onHide={handleClose}>
-              <Modal.Header closeButton>
-                <Modal.Title>Enter Confirmation Number</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <p>Enter the Confirmation Number given to you with your purchase</p>
-                <Form onSubmit={onFormSubmit}>
-                  <FormGroup>
-                  <Form.Control
-                    type="name" 
-                    placeholder="Confirmation Number" 
-                    onChange={(e) => setValue(e.target.value)} 
-                    value={value}
-                    />
-                  </FormGroup>
-                  {alert &&
-                        <Alert style={{ maxWidth: '200px', marginTop:5, paddingTop:'2px', maxHeight:'30px', }} key={alert.type} variant={alert.type}>
-                        {alert.label}
-                        </Alert>
-                    }
-                </Form>
-                <Button  className="me-0" type='submit' variant="success" onClick={onFormSubmit}>                
-                  Enter 
-                </Button>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                  Close
-                </Button>
-                
-              </Modal.Footer>
+
+                        {showData.map((item, index) => (
+                            <tr key={index} style={{ alignItems: 'center' }}>
+                                <td style={{ textAlign: 'center' }}>{item.show.performance}</td>
+                                <td>{item.show.venue} </td>
+                                <td>{item.show.date} </td>
+                                <td>number </td>
+                            </tr>
+                        ))}
+
+                    </tbody>
+                </Table>
+
+            </Stack>
+
+            <Modal show={showModal} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Enter Event Data</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={onFormSubmit} id='newShowForm' >
+                        {formError && <Alert variant='danger'>{formError}</Alert>
+                        }
+                        <Row className="mb-3">
+                            <Form.Group controlId="textValue">
+                                <Form.Label>Event Name</Form.Label>
+                                <Form.Control required type="text" value={formData.performance} placeholder="Enter event name" onChange={handleTextChange} />
+                            </Form.Group>
+                        </Row>
+                        <Row className="mb-3">
+                            <Form.Group as={Col} controlId="selectValue">
+                                <Form.Label>Pick Venue </Form.Label>
+                                <Form.Select required value={formData.venue} onChange={handleSelectChange} >
+                                    <option value="">Select an option</option>
+                                    <option value="Playhouse">Playhouse</option>
+                                    <option value="Concert Hall">Concert Hall</option>
+                                </Form.Select>
+                            </Form.Group>
+
+
+                            <Form.Group as={Col} controlId="dateValue">
+                                <Form.Label>Date</Form.Label>
+                                <Form.Control type='date' required placeholder="MM/DD/YYYY" value={formData.date} onChange={handleDateChange} />
+                            </Form.Group>
+
+                        </Row>
+                        <Button variant="primary" type="submit" >
+                            Submit
+                        </Button>
+
+                    </Form>
+                </Modal.Body>
             </Modal>
-    </div>
-    
-    )   
+        </div>
+
+    )
 }
--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -2,21 +2,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo-tran.png';
 import VBCplaceholder from './VBC.jpg';
 import { Stack, Alert, Image, Form, Card, Button, Modal, FormGroup } from 'react-bootstrap';
-import {Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import { useState } from 'react';
 import '../styles.css';
 
-export const Home = () =>{
+export const Home = () => {
 
   const [showModal, setShow] = useState(false);
-  const [passState, setState] = useState({event:'',venue:''});
+  const [passState, setState] = useState({ event: '', venue: '' });
   const [value, setValue] = useState('');
   const [alert, setAlert] = useState(undefined);
-  
+
   const [selectedShow, setSelectedShow] = useState(null);
 
   const handleSelectShow = (item) => {
-    setState({event:item.show.performance, venue:item.show.venue});
+    setState({ event: item.show.performance, venue: item.show.venue });
   };
 
   const confNumModal = () => {
@@ -24,178 +24,178 @@ export const Home = () =>{
   };
   const handleClose = () => {
     setShow(false)
-    setAlert(undefined); 
+    setAlert(undefined);
   }
 
   const data = [
     {
       "show":
       {
-          "performance": "West Side Story",
-          "venue" : "Civic Center Playhouse",
-          "date": "4/12/2023"
-      }   
+        "performance": "West Side Story",
+        "venue": "Civic Center Playhouse",
+        "date": "4/12/2023"
+      }
     },
     {
       "show":
       {
-          "performance": "Titanic 2",
-          "venue" : "Civic Center Playhouse",
-          "date": "4/13/2023"
-      }   
+        "performance": "Titanic 2",
+        "venue": "Civic Center Playhouse",
+        "date": "4/13/2023"
+      }
     },
     {
       "show":
       {
-          "performance": "Die Hardest",
-          "venue" : "Civic Center Concert Hall",
-          "date": "4/14/2023"
-      },   
+        "performance": "Die Hardest",
+        "venue": "Civic Center Concert Hall",
+        "date": "4/14/2023"
+      },
     },
     {
       "show":
       {
-          "performance": "How I Met Your Mother Abridged",
-          "venue" : "Civic Center Concert Hall",
-          "date": "4/15/2023"
-      },   
+        "performance": "How I Met Your Mother Abridged",
+        "venue": "Civic Center Concert Hall",
+        "date": "4/15/2023"
+      },
     },
     {
       "show":
       {
-          "performance": "West Side Story",
-          "venue" : "Civic Center Playhouse",
-          "date": "4/12/2023"
-      }   
+        "performance": "West Side Story",
+        "venue": "Civic Center Playhouse",
+        "date": "4/12/2023"
+      }
     },
     {
       "show":
       {
-          "performance": "Titanic 2",
-          "venue" : "Civic Center Playhouse",
-          "date": "4/13/2023"
-      }   
+        "performance": "Titanic 2",
+        "venue": "Civic Center Playhouse",
+        "date": "4/13/2023"
+      }
     },
     {
       "show":
       {
-          "performance": "Die Hardest",
-          "venue" : "Civic Center Concert Hall",
-          "date": "4/14/2023"
-      },   
+        "performance": "Die Hardest",
+        "venue": "Civic Center Concert Hall",
+        "date": "4/14/2023"
+      },
     },
     {
       show:
       {
-          performance: "How I Met Your Mother Abridged",
-          venue : "Civic Center Concert Hall",
-          date: "4/15/2023"
-      },   
+        performance: "How I Met Your Mother Abridged",
+        venue: "Civic Center Concert Hall",
+        date: "4/15/2023"
+      },
     },
-    
+
   ]
 
-  
+
   const [isDisabled, setDisabled] = useState(true);
-  
+
   const onFormSubmit = (e) => {
     e.preventDefault();
     setValue(value);
     console.log(value);
-    
+
     const promise = fetch('http://localhost:4000/confNum', {
-        method: 'POST',
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({value})
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ value })
     });
     console.log(promise)
     promise.then(event => {
-        if (event.status === 200) {
-          setAlert({ label: 'success', type: 'success' })
-          //handleClose()
-          //window.location.href="/seatSelection"
-          var x = document.getElementById("continueButton");
-          //un-disable var x
-          setDisabled(false);
-        } else {
-          setAlert({ label: `Error ${event.statusText}`, type: 'danger' })
-        }
-      })
-}
-  
+      if (event.status === 200) {
+        setAlert({ label: 'success', type: 'success' })
+        //handleClose()
+        //window.location.href="/seatSelection"
+        var x = document.getElementById("continueButton");
+        //un-disable var x
+        setDisabled(false);
+      } else {
+        setAlert({ label: `Error ${event.statusText}`, type: 'danger' })
+      }
+    })
+  }
+
   return (
-        <div className='App-body '>
-        <Stack direction='vertical' style={{alignItems:'center'}} gap={0}>
-          <Image src={logo} className='App-logo-big' style={{marginTop:'-30px'}}></Image>
-          <div className="square border border-secondary border-3 container" style={{maxWidth:'95%', maxHeight:'45rem' , padding:'35px',overflowY:'auto', marginBottom:'30px', marginTop:'-70px', background:'#282634'}}>            
-          <Stack className="mb-5 flex-wrap" direction='horizontal' style={{justifyContent:'center'}} gap={3}>
-            {data.map((item,index) => (
-              <Card key={index} style={{ width: '18rem', height:'22rem'}}>
-                <Card.Img height={'50%'} variant="top" src={VBCplaceholder}/>
+    <div className='App-body '>
+      <Stack direction='vertical' style={{ alignItems: 'center' }} gap={0}>
+        <Image src={logo} className='App-logo-big' style={{ marginTop: '-30px' }}></Image>
+        <div className="square border border-secondary border-3 container" style={{ maxWidth: '95%', maxHeight: '45rem', padding: '35px', overflowY: 'auto', marginBottom: '30px', marginTop: '-70px', background: '#282634' }}>
+          <Stack className="mb-5 flex-wrap" direction='horizontal' style={{ justifyContent: 'center' }} gap={3}>
+            {data.map((item, index) => (
+              <Card key={index} style={{ width: '18rem', height: '22rem' }}>
+                <Card.Img height={'50%'} variant="top" src={VBCplaceholder} />
                 <Card.Body>
                   <Card.Title>{item.show.performance}</Card.Title>
                   <Card.Text >
                     {item.show.venue} <br /> {item.show.date}
                   </Card.Text>
                   <Stack direction='horizontal' gap={2}>
-                  <Link 
-                    to={"/seatSelection"}
-                    state={{case:"purchase",event:item.show.performance,venue:item.show.venue, data:item}}>
-                    <Button size='sm' variant="primary" >
-                          Purchase Tickets
-                    </Button>
-                  </Link>
-                    <Button size='sm' variant="primary" onClick={() =>{
-                    confNumModal()
-                    handleSelectShow(item)
+                    <Link
+                      to={"/seatSelection"}
+                      state={{ case: "purchase", event: item.show.performance, venue: item.show.venue, data: item }}>
+                      <Button size='sm' variant="primary" >
+                        Purchase Tickets
+                      </Button>
+                    </Link>
+                    <Button size='sm' variant="primary" onClick={() => {
+                      confNumModal()
+                      handleSelectShow(item)
                     }}>
-                          Exchange Tickets
+                      Exchange Tickets
                     </Button>
-                    </Stack>
+                  </Stack>
                 </Card.Body>
               </Card>
             ))}
           </Stack>
-            
-            <Modal show={showModal} onHide={handleClose}>
-              <Modal.Header closeButton>
-                <Modal.Title>Enter Confirmation Number</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <p>Enter the Confirmation Number given to you with your purchase</p>
-                <Form onSubmit={onFormSubmit}>
-                  <FormGroup>
+
+          <Modal show={showModal} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Enter Confirmation Number</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p>Enter the Confirmation Number given to you with your purchase</p>
+              <Form onSubmit={onFormSubmit}>
+                <FormGroup>
                   <Form.Control
-                    type="name" 
-                    placeholder="Confirmation Number" 
-                    onChange={(e) => setValue(e.target.value)} 
+                    type="text"
+                    placeholder="Confirmation Number"
+                    onChange={(e) => setValue(e.target.value)}
                     value={value}
-                    />
-                  </FormGroup>
-                  {alert &&
-                        <Alert style={{ maxWidth: '200px', marginTop:5, paddingTop:'2px', maxHeight:'30px', }} key={alert.type} variant={alert.type}>
-                        {alert.label}
-                        </Alert>
-                    }
-                </Form>
-                <Button  className="me-0" type='submit' variant="success" onClick={onFormSubmit}>                
-                  Enter 
-                </Button>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                  Close
-                </Button>
-                  <Button id="continueButton" variant="primary" disabled={isDisabled}>
-                    <Link to={"/seatSelection"} style={{color:'white', textDecoration:'none'}} state={{event:passState.event, venue:passState.venue}}>                
-                        Continue 
-                    </Link>
-                  </Button>
-                
-              </Modal.Footer>
-            </Modal>
-          </div>
-        </Stack>
-      </div>
-    )
+                  />
+                </FormGroup>
+                {alert &&
+                  <Alert style={{ maxWidth: '200px', marginTop: 5, paddingTop: '2px', maxHeight: '30px', }} key={alert.type} variant={alert.type}>
+                    {alert.label}
+                  </Alert>
+                }
+              </Form>
+              <Button className="me-0" type='submit' variant="success" onClick={onFormSubmit}>
+                Enter
+              </Button>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button id="continueButton" variant="primary" disabled={isDisabled}>
+                <Link to={"/seatSelection"} style={{ color: 'white', textDecoration: 'none' }} state={{ event: passState.event, venue: passState.venue }}>
+                  Continue
+                </Link>
+              </Button>
+
+            </Modal.Footer>
+          </Modal>
+        </div>
+      </Stack>
+    </div>
+  )
 }
