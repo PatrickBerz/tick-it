345e4792107280b479115a7b52c45670d603e6a4
Nathaniel Smith <cns0030@uah.edu>
2023-04-15 01:42:43 -0500

    TODO: Figure out complete functionality for createPurchase and determine additional necessary functionality.

--- backend/src/System.ts
+++ backend/src/System.ts
@@ -1,13 +1,11 @@
-import { Seat } from "./Seat";
-import { Ticket } from "./Ticket";
-import { Performance } from "./Performance";
-import { SeatSection } from "./SeatSection";
-import { Attendee } from "./Attendee";
 import { Venue } from "./Venue";
 import { SeasonTicketHolder } from "./SeasonTicketHolder";
 import { Show } from "./Show";
 import { Purchase } from "./Purchase";
+import { Attendee } from "./Attendee";
+import { JSONHandler } from "../JSONHandler";
 export class System {
+    private deserializer : JSONHandler = new JSONHandler();
     private venues : Venue[] = [];
     private shows : Show[] = [];
     private purchases : Purchase[] = [];
@@ -20,10 +18,34 @@ export class System {
         this.seasonTicketHolders = this.initializeSeasonHolders(filePath);
     }
 
-    private initializeVenues(filePath : string) : Venue[] {return []} //temporary
-    private initializeShows(filePath : string) : Show[] {return []}
-    private initializePurchases(filePath : string) : Purchase[] {return []}
-    private initializeSeasonHolders(filePath : string) : SeasonTicketHolder[] {return []}
+    private initializeVenues(filePath : string) : Venue[] 
+    {
+        this.deserializer.deserializeVenue(filePath);
+        return this.deserializer.getData();
+    }
 
-    createPurchase(){}
+    private initializeShows(filePath : string) : Show[] 
+    {
+        this.deserializer.deserializeShow(filePath);
+        return this.deserializer.getData();
+    }
+
+    private initializePurchases(filePath : string) : Purchase[] 
+    {
+        this.deserializer.deserializePurchase(filePath);
+        return this.deserializer.getData();
+    }
+
+    private initializeSeasonHolders(filePath : string) : SeasonTicketHolder[] 
+    {
+        this.deserializer.deserializeSeasonTicketHolder(filePath);
+        return this.deserializer.getData();
+    }
+
+    createPurchase(name:string, address:string, phoneNum:string) : Purchase 
+    {
+        let newPurchase = new Purchase(new Attendee(name, address, phoneNum));
+        this.purchases.push(newPurchase);
+        return newPurchase;
+    }
 }
