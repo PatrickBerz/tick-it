b2f6c914b9a1220102175a8de950f115f6482551
PatrickB <patrickberzins14@gmail.com>
2023-04-16 21:28:00 -0500

    nesting

--- frontend/src/components/seasonPassStuff.jsx
+++ frontend/src/components/seasonPassStuff.jsx
@@ -141,8 +141,8 @@ export const SeasonPassStuff = () => {
             <div className='border border-light-2' style={{ maxWidth: '100%', maxHeight: '100%', alignSelf: 'center', marginTop: '60px', paddingLeft: '25px', paddingRight: '25px' }}>
 
                 <Stack direction='vertical' style={{ marginTop: '40px' }} gap={2}>
-                <div className='d-flex ' style={{ width: '95%', alignSelf: 'center' }}>                        
-                <Button className=' p-2' style={{
+                    <div className='d-flex ' style={{ width: '95%', alignSelf: 'center' }}>
+                        <Button className=' p-2' style={{
                             borderColor: '#FF4057',
                             backgroundColor: '#FF4057',
                         }} // send file path
