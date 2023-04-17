fa013a5448ca0f2d57cc362458bd49f3f182b0a2
PatrickB <patrickberzins14@gmail.com>
2023-04-16 21:40:25 -0500

    removed white border

--- frontend/src/components/ticketStuff.jsx
+++ frontend/src/components/ticketStuff.jsx
@@ -31,7 +31,7 @@ export const TicketStuff = () => {
     if (data) {
         console.log(JSON.stringify(data))
         return (
-            <div className='border border-light-2' style={{ maxWidth: '100%', maxHeight: '100%', alignSelf: 'center', marginTop: '60px', paddingLeft: '25px', paddingRight: '25px' }}>
+            <div className='d-flex' style={{ maxWidth: '100%', maxHeight: '100%', alignSelf: 'center', marginTop: '60px', paddingLeft: '25px', paddingRight: '25px' }}>
                 <Stack direction='vertical' style={{ marginTop: '40px' }} gap={2}>
 
                     <div className='d-flex ' style={{ width: '95%', alignSelf: 'center' }}>
