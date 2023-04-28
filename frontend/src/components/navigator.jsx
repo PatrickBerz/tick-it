1c0ebc1307e60e05a79ad1d99726f38db4201297
Van H <sgrace2701@gmail.com>
2023-04-28 15:18:28 -0500

    checkout done

--- frontend/src/components/navigator.jsx
+++ frontend/src/components/navigator.jsx
@@ -1,6 +1,6 @@
-import React, {Component } from 'react';
-import {Navbar, Nav, Container} from 'react-bootstrap';
-import {Route, Routes, Link } from 'react-router-dom';
+import { Component } from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 
 export default class Navigator extends Component{
